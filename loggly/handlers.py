4301f85ce8dbc9c4991900b67f2bb6ec47cad754
psquickitjayant <jayantvarshney018@gmail.com>
2016-05-11 15:42:11 +0530

    Update handlers.py

--- loggly/handlers.py
+++ loggly/handlers.py
@@ -1,18 +1,44 @@
 import logging
 import logging.handlers
-
 import socket
 import traceback
-
+import json
+import pytz
+from datetime import datetime
+from tzlocal import get_localzone
 from requests_futures.sessions import FuturesSession
 
 session = FuturesSession()
-
 
 def bg_cb(sess, resp):
     """ Don't do anything with the response """
     pass
 
+# add utc in timestamp
+def get_utc_date(local_date):
+    datefrmt = datetime.strptime(str(local_date), '%Y-%m-%d %H:%M:%S,%f')
+    tz = get_localzone()
+    local_dt = tz.localize(datefrmt, is_dst=None)
+    utc_dt = local_dt.astimezone(pytz.utc)
+    return utc_dt.isoformat()
+
+# chack data is valid json or not
+def is_json(data):
+    try:
+        json_object = json.loads(data)
+    except ValueError, e:
+        return False
+    return True
+
+# convert log to Json Data if log have key value pair
+def format_timestamp(log):
+    jsondata = json.loads(log)
+    if 'timestamp' in jsondata and jsondata['timestamp'] is not None:
+        jsondata['timestamp'] = get_utc_date(jsondata['timestamp'])
+    else:
+        # if time stamp not present added timestamp with current utc time
+        jsondata['timestamp'] = datetime.utcnow().isoformat()
+    return jsondata
 
 class HTTPSHandler(logging.Handler):
     def __init__(self, url, fqdn=False, localname=None, facility=None):
@@ -31,8 +57,11 @@ class HTTPSHandler(logging.Handler):
     def emit(self, record):
         try:
             payload = self.format(record)
+            if is_json(payload):
+                payload = format_timestamp(payload)
             session.post(self.url, data=payload, background_callback=bg_cb)
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
             self.handleError(record)
+            
